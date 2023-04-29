d50f9c8f9a1ad117462132f2fb74f8e83ef55b07
ikorotkaya <irinkakorotkaya@gmail.com>
2023-04-29 15:48:53 +0200

    Add try-catch for customers and authors routes

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -2,21 +2,42 @@ const express = require('express');
 const authorsRouter = express.Router();
 const config = require('../config/config');
 
+const bodyParser = require('body-parser');
+authorsRouter.use(bodyParser.json());
+authorsRouter.use(bodyParser.urlencoded({ extended: true }));
+
 // Get all authors
 authorsRouter.get('/', async (req, res) => {
-  const result = await config.runQuery('SELECT * from author')
-  
-  res.send({
-    authors: result.rows
-  })
+  try {
+    const result = await config.runQuery('SELECT * from authors')
+    res.send({
+      authors: result.rows
+    })
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 // Get authors by id
 authorsRouter.get('/:id', async (req, res) => {
-  const userId = req.params.id;
-  const result = await config.runQuery(`SELECT * from authors WHERE id = ${userId}`)
-  
-  res.send(result.rows)
+  try {
+    const userId = req.params.id;
+    const result = await config.runQuery(`SELECT * from authors WHERE id = ${userId}`)
+    res.send(result.rows)
+  } catch (err) {
+    res.status(400).json(err)
+  }
+});
+
+// Create new author
+authorsRouter.post('/', async (req, res) => {
+  try {
+    const { id, first_name, last_name } = req.body
+    await config.runQuery(`INSERT INTO authors (id, first_name, last_name) VALUES ('${id}', ${first_name}', '${last_name}')`)
+    res.status(201).send('Author added')
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 module.exports = authorsRouter;
--- src/routes/customers.js
+++ src/routes/customers.js
@@ -4,39 +4,41 @@ const config = require('../config/config');
 
 const bodyParser = require('body-parser');
 customersRouter.use(bodyParser.json());
-customersRouter.use(bodyParser.urlencoded({extended: true}));
+customersRouter.use(bodyParser.urlencoded({ extended: true }));
 
 // Get all customers
 customersRouter.get('/', async (req, res) => {
-  const result = await config.runQuery('SELECT * from customers')
-
-  res.send({
-    customers: result.rows
-  })
+  try {
+    const result = await config.runQuery('SELECT * from customers')
+    res.send({
+      customers: result.rows
+    })
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 // Create new customer
 customersRouter.post('/', async (req, res) => {
-  if (!req.body) return res.sendStatus(400)
-  const {id, username, password, email} = req.body
-  
-  await config.runQuery(`INSERT INTO customers (id, username, password, email) VALUES ('${id}', '${username}', '${password}', '${email}')`)
-  const updatedResult = await config.runQuery('SELECT * from customers')
-  
-  res.status(201).send('User added with ID: ' + JSON.stringify(updatedResult.rows))
+  try {
+    const { id, username, email, password } = req.body
+    await config.runQuery(`INSERT INTO customers (id, username, password, email) VALUES ('${id}', '${username}', '${password}', '${email}')`)
+    res.status(201).send('User added')
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 // Update username 
 customersRouter.put('/:id', async (req, res) => {
-  if (!req.body) return res.sendStatus(400)
-  
-  const { id } = req.params;
-  const { username } = req.body
-  
-  await config.runQuery(`UPDATE customers SET username = '${username}' WHERE id = '${id}'`)
-  const updatedResult = await config.runQuery(`SELECT * from customers where id = '${id}'`)
-  
-  res.status(201).send('Email updated' + JSON.stringify(updatedResult.rows))
+  try {
+    const { id } = req.params;
+    const { username } = req.body
+    await config.runQuery(`UPDATE customers SET username = '${username}' WHERE id = '${id}'`)
+    res.status(201).send('Email updated')
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 module.exports = customersRouter;
